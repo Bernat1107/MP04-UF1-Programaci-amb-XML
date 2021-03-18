4253aa712dbe7dcc0d52c1b585f4aef4a86d563d
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2021-03-18 17:13:54 +0100

    Update upload.php

---
+++ UF2_AC1/uytransfer/upload.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="es">
+	<head>
+		<meta charset="utf-8">
+		<title>uytransfer</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+
+	<body>
+		
+
+
+
+
+
+
+
+		
+	</body>

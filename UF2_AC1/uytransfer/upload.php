abb9380b947972543caa9de610bdf88e202e9108
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2021-03-22 23:29:26 +0100

    Update upload.php

--- UF2_AC1/uytransfer/upload.php
+++ UF2_AC1/uytransfer/upload.php
@@ -1,40 +1,54 @@
 <!doctype html>
-<html lang="es">
+<html>
 	<head>
 		<meta charset="utf-8">
-		<title>uytransfer</title>
+		<title>SUBIR ARCHIVO</title>
 		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 
 	<body>
-			
-		<?php
+		 <?php
+		    include "header.php";		
+	        define("COLOR_1", "#9e9e9e");
+			const COLOR_2 = "pink";
+		    $nompersona = $_POST["nombre"];
+			if (!empty($_FILES)) {
 
-			print_r($_POST);
-			print_r($_FILES);
+				$nombre = $_FILES["archivo"]["name"];
+				$rutaTmp = $_FILES["archivo"]["tmp_name"];
+				 
+				$extension = substr($nombre, strpos($nombre, "."));
+				$rutaDestino = "files/".date("Y").date("m").date("d").rand(10000,99999).$extension;
+				$linkDescarga = $_SERVER["HTTP_ORIGIN"].$rutaDestino;
 
+				
+				move_uploaded_file($rutaTmp, $rutaDestino);
 
-			if (!empty($_POST)) {
+				echo "<br><p style=\"float: left; margin: 100px; \"> <img src=\"imatges/correcto.jpg\" /> </p>";
 
-				$nombre = $_POST["nombre"];
+                echo "<p style=\"float: left; font-size: 25px;\">ARCHIVO ENVIADO CORRECTAMENTE </p>";
 
-				echo "<p> Hola $nombre </p>";
+                
+                if (empty($nompersona) ) {
 
-				$nombreArchivo = $_FILES["cosaparasubir"]["name"];
+                   echo "<div style=\"float: left;  margin-right: 5px; width: 600px; height: 50px;font-size: 25px; \"> 
+                    Oye tú, usa este link para compartir el fichero </div>";
+	      
+                }
+                else
+                {
+                 
+                    echo "<div style=\" float: left; margin-right: 5px; width: 600px; height: 50px;font-size: 25px; \"> 
+                     Hola $nompersona , usa este link para compartir tu archivo  </div>";
+                }
 
-				if(isset($_POST["portmail"]) {
-
-				echo "se tiene que enviar un email";
-				}
-
+				echo "<p style=\" float: left; margin-right: 5px; width: 900px; height: 50px;font-size: 25px; \"> <a href=\"$rutaDestino\">$linkDescarga</a></p>";
 			}
 
-			else {
-				header("Location: index.php");
-			}
+
 		?>
-
-
 	</body>
 </html>
+           
+			 

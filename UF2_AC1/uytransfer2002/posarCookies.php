ea1b6e436d63299385e98ad0c05b4aa6e6b0fea7
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2021-03-25 16:02:08 +0100

    Create posarCookies.php

---
+++ UF2_AC1/uytransfer2002/posarCookies.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>SUBIR ARCHIVO</title>
+		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+
+	<body>
+
+
+
+
+
+
+
+
+
+
+
+	</body>
+
+	
+</html>
+           

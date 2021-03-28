45ad058b0d3ba081bff5785014518ca6888d52e1
Bernat1107 <79994724+Bernat1107@users.noreply.github.com>
2021-03-28 21:22:07 +0200

    Update mostrarCookies.php

--- UF2_AC1/uytransfer/mostrarCookies.php
+++ UF2_AC1/uytransfer/mostrarCookies.php
@@ -2,31 +2,36 @@
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>SUBIR ARCHIVO</title>
+		<title>MOSTRAR COOKIES</title>
 		<link rel="stylesheet" type="text/css" href="css/estilos.css" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	</head>
 
 	<body>
 		 <?php
+		   	
+            
 		   
-		    include "header.php";	
+		    if (isset($_COOKIE["numlinks"]))
+		     {
 
-		   
-		    if (isset($_COOKIE["numlinks"])) {
-
+                 
 		    	$numlinks = $_COOKIE["numlinks"];
 		    	
+              
 
-		    	while(isset($_COOKIE["numlinks"]))
+		    	while (isset($_COOKIE["link$numlinks"])) 
+		    	{
 
-		    		$numlinks++;
-
+                $link = $_COOKIE["link$numlinks"];
+                echo "<p> <a href=\"$link\"> $link </a> </p>";
+                $numlinks--;
+        
+		    	}            
 
 		    }
-
+         
 		   
-
 		?>
 		    
 	</body>
